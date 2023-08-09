5b819048c96559d92f591f5a1d5ed533af7e5404
kalai1230 <114598413+kalai1230@users.noreply.github.com>
2023-08-09 19:31:21 +0530

    Update hello.py
    checking addition

--- hello.py
+++ hello.py
@@ -1,1 +1,11 @@
 print("Hello World")
+# Simple addition program in Python
+
+def add_numbers(a, b):
+    return a + b
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+result = add_numbers(num1, num2)
+print("The sum of", num1, "and", num2, "is:", result)
